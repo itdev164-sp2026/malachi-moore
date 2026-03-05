4f3fea77bc9039f5bc5dc63dd2d4a1412dbc5287
Malachi-M12 <moorm129@matc.edu>
2026-03-05 01:30:04 -0600

    Restored from.

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -2,22 +2,22 @@ import * as React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
-const styledHeader = styled.header`
+const StyledHeader = styled.header`
   margin: 0 auto;
   padding: var(--space-4) var(--size-gutter);
   display: flex;
   align-items: center;
   justify-content: space-between;
 `
-const styledLink = styled(Link)`
+const StyledLink = styled(Link)`
   font-size: var(--font-sm);
   text-decoration: none;
 `
 
 const Header = ({ siteTitle }) => (
-  <styledHeader>
-    <styledLink to="/">{siteTitle}</styledLink>
-  </styledHeader>
+  <StyledHeader>
+    <StyledLink to="/">{siteTitle}</StyledLink>
+  </StyledHeader>
 )
 
 export default Header
